65aad937b908d5e25a453240b5c54404505f1a47
chinaxxren <jiangmingz@qq.com>
2024-06-11 14:05:18 +0800

    first commit

---
+++ PlayerDemo/PlayerDemo/Sources/Bridge/PlayerDemo-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  PlayerDemo-Bridging-Header.h
+//  PlayerDemo
+//
+//  Created by 赵江明 on 2024/6/11.
+//  Copyright © 2024 chenp. All rights reserved.
+//
+
+#ifndef PlayerDemo_Bridging_Header_h
+#define PlayerDemo_Bridging_Header_h
+
+#import "Net.h"
+#import "UINavigationController+Autorotate.h"
+#import "UITabBarController+Autorotate.h"
+#import "UIViewController+Autorotate.h"
+
+#endif /* PlayerDemo_Bridging_Header_h */
